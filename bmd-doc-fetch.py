680f5c0e217a0e729237ff0f1ada4bf36342df3b
mraerino <melodie124@gmail.com>
2014-08-24 01:05:23 +0200

    v1.0

--- bmd-doc-fetch.py
+++ bmd-doc-fetch.py
@@ -9,13 +9,17 @@ download_url = "http://www.blackmagicdesign.com/api/support/us/downloads.json"
 nav_url = "http://www.blackmagicdesign.com/api/support/us/nav.json"
 path = "C:\\Users\\Marcus\\BILDQUADRAT\\BMD_Download"
 
+class MyURLopener(urllib.request.FancyURLopener):
+    version = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0"
+
+urlOpener = MyURLopener()
+
 with urllib.request.urlopen(download_url) as req:
     struc = json.loads(str(req.read(), encoding='UTF-8'))['downloads']
 with urllib.request.urlopen(nav_url) as req:
     nav = json.loads(str(req.read(), encoding='UTF-8'))
 
 for obj in struc:
-    print(obj['name'])
     common_name = html.unescape(nav['families'][obj['relatedFamilies'][0]]['family_name'].replace('<br>', ' '))
     common_dir = path + "\\" + common_name
     dir_name = common_dir + "\\" + obj['name']
@@ -28,12 +32,10 @@ for obj in struc:
         os.mkdir(common_dir)
     os.mkdir(dir_name)
 
-    downloads = {}
+    downloads = []
     for platform in obj['platforms']:
-        os.mkdir(dir_name + "\\" + platform)
-        downloads[platform] = []
         if 'readme' in obj['urls'][platform]:
-            downloads[platform].append("http:" + obj['urls'][platform]['readme'])
+            downloads.append("http:" + obj['urls'][platform]['readme'])
         # Downloadlink per api holen
         link_url = "http://www.blackmagicdesign.com/api/register/us/download/" + str(obj['urls'][platform]['downloadId'])
         payload = '{"country":"us","platform":"' + platform + '"}'
@@ -43,9 +45,12 @@ for obj in struc:
         req = urllib.request.Request(link_url, bytearray(payload, 'utf-8'), {'Content-type': 'application/json'})
         try:
             with urllib.request.urlopen(req) as resp:
-                downloads[platform].append(str(resp.read(), encoding='UTF-8'))
+                downloads.append(str(resp.read(), encoding='UTF-8'))
         except:
             print("Could not load '" + obj['name'] + "'")
 
-    print(downloads)
+    for url in downloads:
+        print("Downloading " + obj['name'] + "...")
+        print(url)
+        urlOpener.retrieve(url, dir_name + "\\" + os.path.basename(url))
 
